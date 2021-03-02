346b3ace62b7754e2b61fe14e3dcb8fde662f488
Quyen Luu <qluu8268@sdsu.edu>
2021-03-02 14:39:10 -0800

    add files

---
+++ alarmsystem/testmagnetic_mar02a.ino
@@ -0,0 +1,22 @@
+const int buzzer = 4; 
+const int sensor = 2;
+
+int state; // 0 close - 1 open wwitch
+
+void setup()
+{
+  pinMode(sensor, INPUT_PULLUP);
+}
+
+void loop()
+{
+  state = digitalRead(sensor);
+  
+  if (state == HIGH){
+    tone(buzzer, 1000);
+  }
+  else{
+    noTone(buzzer);
+  }
+  delay(200);
+}
